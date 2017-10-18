2355d70bd306ec1ba0f281cd2378f1e904464523
totoroliu <aser2589@gmail.com>
2017-10-18 23:50:05 +0800

    add token

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,10 +2,10 @@ import api from "../api";
 import {userLoggedIn} from "./auth";
 
 export const signup = data => dispatch =>
-api.user.signup(data).then(user => 
-    dispatch(userLoggedIn(user))
-);
-
+api.user.signup(data).then(user => {
+    localStorage.JWTtoken = user.JWTtoken;
+    dispatch(userLoggedIn(user));
+});
 
 
     
