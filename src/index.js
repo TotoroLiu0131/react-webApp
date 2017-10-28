cb904a215953ebad38afd2f19678d7beb2b4fa81
totoroliu <aser2589@gmail.com>
2017-10-28 23:32:16 +0800

    add

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css' ;
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux" ;
 import thunk from "redux-thunk";
+import decode from "jwt-decode";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from "./rootReducer";
@@ -17,7 +18,12 @@ const store = createStore(
 );
 
 if (localStorage.JWTtoken){
-    const user = {token: localStorage.JWTtoken};
+    const payload = decode(localStorage.JWTtoken);
+    const user = {
+        token: localStorage.JWTtoken, 
+        email: payload.email, 
+        confirmed: payload.confirmed
+    };
     store.dispatch(userLoggedIn(user));
 }
 
